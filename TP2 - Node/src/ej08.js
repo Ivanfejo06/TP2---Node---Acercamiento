e097530ba953a35bacd4a910c72754116baf71fa
Ivanfejo06 <128491414+Ivanfejo06@users.noreply.github.com>
2024-04-10 09:01:51 -0300

    Update ej08.js

--- TP2 - Node/src/ej08.js
+++ TP2 - Node/src/ej08.js
@@ -2,45 +2,29 @@ import axios from "axios";
 import https from "https"; // Importa el módulo 'https'
 
 const APIKEY = "a7d58067";
-const Test = async (searchText) => {
-    const requestString = `https://www.omdbapi.com/?apikey=${APIKEY}&s=cars`;
+
+const OMDBSearchByPage = async (searchText, page) => {
+    const requestString = `https://www.omdbapi.com/?apikey=${APIKEY}&s=${searchText}&page=${page}`;
     const apiResponse = await axios.get(requestString, {
         httpsAgent: new https.Agent({ rejectUnauthorized: false }) // Ignora la validación del certificado SSL
     });
     return apiResponse.data;
 };
 
-let respuesta = await Test();
-console.log('respuesta', respuesta);
-
-const OMDBSearchByPage = async (searchText, page = 1) => {
-let returnObject = {
-    respuesta : false,
-    cantidadTotal : 0,
-    datos : {}
-};
-
-// hacer la función
-return returnObject;
-};
 const OMDBSearchComplete = async (searchText) => {
-let returnObject = {
-    respuesta : false,
-    cantidadTotal : 0,
-    datos : {}
+    const requestString = `https://www.omdbapi.com/?apikey=${APIKEY}&s=${searchText}`;
+    const apiResponse = await axios.get(requestString, {
+        httpsAgent: new https.Agent({ rejectUnauthorized: false }) // Ignora la validación del certificado SSL
+    });
+    return apiResponse.data;
 };
 
-// hacer la función
-return returnObject;
-};
 const OMDBGetByImdbID = async (imdbID) => {
-let returnObject = {
-    respuesta : false,
-    cantidadTotal : 0,
-    datos : {}
+    const requestString = `https://www.omdbapi.com/?apikey=${APIKEY}&i=${imdbID}`;
+    const apiResponse = await axios.get(requestString, {
+        httpsAgent: new https.Agent({ rejectUnauthorized: false }) // Ignora la validación del certificado SSL
+    });
+    return apiResponse.data;
 };
-// hacer la función
-return returnObject;
-};
-// hacer la función
+
 export {OMDBSearchByPage, OMDBSearchComplete, OMDBGetByImdbID};
